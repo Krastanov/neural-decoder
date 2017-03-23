Refactor sampling and fix a bug from the previous commit (in de-normalizing the stabs).

neural.py
@@ -119,7 +119,7 @@ def data_generator(H, out_dimZ, out_dimX, in_dim, p, batch_size=512, size=None,
         c += 1
         if size and c==size:
             raise StopIteration
-            
+
 def do_normcenterstab(stabs, p):
     avg = (1-p)*2/3
     avg_stab = 4*avg*(1-avg)**3 + 4*avg**3*(1-avg)
@@ -141,3 +141,22 @@ def undo_normcentererr(flips, p):
     avg = (1-p)*2/3
     var = avg-avg**2
     return flips*var**0.5 + avg
+
+def smart_sample(H, stab, pred, sample, giveup):
+    '''Sample `pred` until `H@sample==stab`.
+
+    `sample` is modified in place. `giveup` attempts are done at most.
+    Returns the number of attempts.'''
+    npany = np.any
+    nprandomuniform = np.random.uniform
+    npsum = np.sum
+    npdot = np.dot
+    npnot_equal = np.not_equal
+    attempts = 1
+    mismatch = stab!=npdot(H,sample)%2
+    while npany(mismatch) and attempts < giveup:
+        propagated = npany(H[mismatch,:], axis=0)
+        sample[propagated] = pred[propagated]>nprandomuniform(size=npsum(propagated))
+        mismatch = stab!=npdot(H,sample)%2
+        attempts += 1
+    return attempts

train_network.py
@@ -48,7 +48,7 @@ parser.add_argument('--Xstab', action='store_true',
 args = parser.parse_args()
 print(args)
 
-from neural import create_model, data_generator, do_normcenterstab, undo_normcentererr
+from neural import create_model, data_generator, do_normcenterstab, undo_normcentererr, smart_sample
 from codes import ToricCode
 import numpy as np
 import tqdm
@@ -123,7 +123,6 @@ if args.eval:
     outlen = 2*L**2*(args.Zstab+args.Xstab)
     inlen = L**2*(args.Zstab+args.Xstab)
     c = cz = cx = 0
-    giveup = args.giveup
     if args.trainset:
         stabflipgen = zip(x_test, y_test)
         size = len(y_test)
@@ -132,37 +131,22 @@ if args.eval:
         stabflipgen = data_generator(H, out_dimZ, out_dimX, in_dim, args.prob, batch_size=1, size=size)
     full_log = np.zeros((size, E.shape[0]+args.Zstab+args.Xstab), dtype=int)
     for i, (stab, flips) in tqdm.tqdm(enumerate(stabflipgen), total=size):
-        stab.shape = 1, inlen # TODO this should be unnecessary
         if args.normcenterstab:
-            stab = do_normcenterstab(stab, args.prob)
-        pred = model.predict(stab).ravel() # TODO those seem like unnecessary shape changes
+            stab_normed = do_normcenterstab(stab, args.prob)
+            pred = model.predict(stab_normed).ravel()
+        else:
+            pred = model.predict(stab).ravel()
         if args.normcentererr:
             pred = undo_normcentererr(pred, args.prob)
+        stab = stab.ravel()
+        flips = flips.ravel()
         sample = pred>np.random.uniform(size=outlen)
         if both:
-            attemptsZ = 1
-            attemptsX = 1
-            mismatchZ = stab[0,:inlen//2]!=Hz.dot(sample[:outlen//2])%2
-            while np.any(mismatchZ) and attemptsZ < giveup:
-                propagatedZ = np.any(Hz[mismatchZ,:], axis=0)
-                sample[:outlen//2][propagatedZ] = pred[:outlen//2][propagatedZ]>np.random.uniform(size=np.sum(propagatedZ))
-                mismatchZ = stab[0,:inlen//2]!=Hz.dot(sample[:outlen//2])%2
-                attemptsZ += 1
-            mismatchX = stab[0,inlen//2:]!=Hx.dot(sample[outlen//2:])%2
-            while np.any(mismatchX) and attemptsX < giveup:
-                propagatedX = np.any(Hx[mismatchX,:], axis=0)
-                sample[outlen//2:][propagatedX] = pred[outlen//2:][propagatedX]>np.random.uniform(size=np.sum(propagatedX))
-                mismatchX = stab[0,inlen//2:]!=Hx.dot(sample[outlen//2:])%2
-                attemptsX += 1
+            attemptsZ = smart_sample(Hz, stab[:inlen//2], pred[:outlen//2], sample[:outlen//2], args.giveup)
+            attemptsX = smart_sample(Hx, stab[inlen//2:], pred[outlen//2:], sample[outlen//2:], args.giveup)
         else:
-            attempts = 1
-            mismatch = stab.ravel()!=H.dot(sample)%2 # TODO this also seems like an unnecessary ravel
-            while np.any(mismatch) and attempts < giveup:
-                propagated = np.any(H[mismatch,:], axis=0)
-                sample[propagated] = pred[propagated]>np.random.uniform(size=np.sum(propagated))
-                mismatch = stab.ravel()!=H.dot(sample)%2
-                attempts += 1
-        errors = E.dot((sample+flips.ravel())%2)%2 # TODO this also seems like an unnecessary ravel
+            attempts = smart_sample(H, stab, pred, sample, args.giveup)
+        errors = E.dot((sample+flips)%2)%2
         if np.any(errors) or np.any(stab!=H.dot(sample)%2):
             c += 1
             if both: